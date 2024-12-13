77f157fb9e546fa24e925be0d15b6e6ccb98a881
Asadtop4ik <asadbek.backend@gmail.com>
2024-12-13 11:33:11 +0500

    new

--- config/celery.py
+++ config/celery.py
@@ -13,6 +13,6 @@ app.conf.broker_connection_retry_on_startup = True
 app.conf.beat_schedule = {
     'monthly-payment-task': {
         'task': 'time_schedule.tasks.process_monthly_payments_task',
-        'schedule': crontab(day_of_month=1, hour=0, minute=0),  # First day of each month at midnight
+        'schedule': crontab(day_of_month=1, hour=0, minute=0),
     },
 }
